1991870e37e2c9fa44af4958cd41b211b094b9f7
robinsondev428 <princerobinson428withmail@gmail.com>
2020-08-05 21:33:30 -0600

    [add][evaluationdb]connection with server

--- evaluation-db/src/config/typeorm.config.ts
+++ evaluation-db/src/config/typeorm.config.ts
@@ -4,12 +4,15 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
  */
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'mongodb',
-    host: 'localhost',
-    port: 27017,
+    host: 'evaluation-mongo-2020.mongo.cosmos.azure.com',
+    port: 10255,
+    username: 'evaluation-mongo-2020',
+    password:'QMJUWmzvZM5gZ7b9HTlV3vulIif3fLXv9EdPI1Vo2CBCDZ5op4H5lxScZxMpv06ONgKcAeeWv0gTEZ81B4XnGg==',
     database:  'evaluation',
     entities: [
         'dist/**/*.entity.js',
         './**/*.entity.js'],
     synchronize: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true,
+    ssl: true
 }
