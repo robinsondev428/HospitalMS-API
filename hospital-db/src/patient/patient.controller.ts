806a2524e9d90ff4c5629cb61dccb22f181000f8
robinsondev428 <princerobinson428withmail@gmail.com>
2020-08-10 19:56:28 -0600

    Cotec Sync Implemented
    @estalvgs1999

--- hospital-db/src/patient/patient.controller.ts
+++ hospital-db/src/patient/patient.controller.ts
@@ -1,15 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Controller, Get, Post, Body, Param, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UsePipes, ValidationPipe, HttpService } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse, ApiBody, ApiParam, ApiNotFoundResponse } from '@nestjs/swagger';
 import { PatientService } from './patient.service';
 import { PatientDTO } from './dto/patient.dto';
 import { CreatePatientDTO } from './dto/create-patient.dto';
 import { Patient } from './patient.entity';
+import * as https from 'https';
 
 @ApiTags('Patient')
 @Controller('patient')
 export class PatientController {
-    constructor(private patientService: PatientService){}
+    constructor(private patientService: PatientService, private http: HttpService){}
     /**
      * Get all the patients
      */
@@ -18,6 +20,30 @@ export class PatientController {
     @ApiResponse({ status: 200, type: [PatientDTO] })
     getPatients(){
         return this.patientService.getAllPatient();
+    }
+
+    @Get('/sync')
+    syncCotecAPI() {
+        const patients = this.getCotecData();
+        
+        return { status:'Successful sync'};
+    }
+
+    async getCotecData() {
+        const axios = require('axios');
+        let patients: CreatePatientDTO[];
+        await axios.get('https://localhost:5001/api/v1/patients/crc',{
+            httpsAgent: new https.Agent({
+              rejectUnauthorized: false
+            })
+        }).then(resp => { patients= resp.data;});
+        
+        patients.forEach(
+            pat => {
+                console.log(pat);
+            }
+        );
+        return patients;
     }
 
     /**
--- hospital-db/src/patient/patient.module.ts
+++ hospital-db/src/patient/patient.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { PatientController } from './patient.controller';
 import { PatientService } from './patient.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,6 +7,10 @@ import { PatientRepository } from './patient.repository';
 @Module({
   imports: [
     TypeOrmModule.forFeature([PatientRepository]),
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
   ],
   controllers: [PatientController],
   providers: [PatientService]
