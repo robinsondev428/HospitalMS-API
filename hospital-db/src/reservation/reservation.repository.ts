81b40194f8631085b06d5fc6cef8f510cbe92d7e
robinsondev428 <princerobinson428withmail@gmail.com>
2020-08-12 16:21:13 -0600

    Fixed update reservation bug

--- hospital-db/src/reservation/reservation.repository.ts
+++ hospital-db/src/reservation/reservation.repository.ts
@@ -2,6 +2,7 @@
 import { Repository, EntityRepository, Connection } from "typeorm";
 import { Reservation } from "./reservation.entity";
 import { ReservationDTO } from "./dto/reservation.dto";
+import { MedicalProcedure } from 'src/medical-procedure/medical-procedure.entity';
 @EntityRepository(Reservation)
 export class ReservationRepository extends Repository<Reservation>{
     constructor(private connection: Connection) {
@@ -17,8 +18,13 @@ export class ReservationRepository extends Repository<Reservation>{
         console.log('createReservation', reservation);
         return await reservation.save();
     }
-    async createProcedure(procedure){
-        const procedureReservation = this.connection.getRepository(Reservation);
+
+    async deleteProcedures(procedures: MedicalProcedure[], id: string){
+
+        procedures.forEach( async prod => {
+            const result = await this.query(`call delete_procedures(${id},${prod.id})`);
+            console.log(result);
+        });
     }
 }
 
